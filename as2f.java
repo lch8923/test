afa42ec579006a9d7b54c964b75733fc3a18db76
lch8923 <66227715+lch8923@users.noreply.github.com>
2020-06-01 09:37:29 +0900

    Add files via upload

---
+++ as2f.java
@@ -0,0 +1,22 @@
+package asd2;
+class Employee {
+	String name;
+	int idNum;
+	int salary;
+	boolean sex;
+}
+public class as2f {
+	public static void main(String[] args) {
+			Employee myJik = new Employee();
+			myJik.name = "ȫ�浿";
+			myJik.idNum = 17001;
+			myJik.salary = 4500000;
+			myJik.sex = true;
+			System.out.printf("%s\n", myJik.name);
+			System.out.printf("%d\n", myJik.idNum);
+			System.out.printf("%d\n", myJik.salary);
+			System.out.printf("%d\n", myJik.sex);
+		}
+	}
+
+
---
+++ asrf3.java
@@ -0,0 +1,9 @@
+package asd2;
+
+public class asrf3 {
+	public static void main(String[] args) {
+		
+		}
+	}
+
+
