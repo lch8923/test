406dc7b243b206aa9b8a38b40a95c3cb482684e7
lch8923 <66227715+lch8923@users.noreply.github.com>
2020-06-08 09:11:36 +0900

    Add files via upload

---
+++ a2.java
@@ -0,0 +1,14 @@
+package a;
+
+public class a2 {
+
+	public static void main(String[] args) {
+		int sum=0;
+		for(int i=0;i<=10;i++){
+			sum+=i;
+			System.out.println(i);
+		}
+		System.out.println(sum);
+	}
+
+}
